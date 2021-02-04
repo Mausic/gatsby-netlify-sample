5e699ba62f499996718e9cfb43bd5b3b1b2addd2
Maksym Ryndia <ryndya.maxim@gmail.com>
2021-02-04 17:46:51 +0200

    Setup gatsby to work with *.md files

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,6 +28,59 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    // include our news *.md files
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `news`,
+        path: `${__dirname}/content/news`,
+      },
+    },
+    // include our netlify cms assets
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `assets`,
+        path: `${__dirname}/content/assets`,
+      },
+    },
+    // set up *.md files processing
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        extensions: [".mdx", ".md"],
+        gatsbyRemarkPlugins: [
+          // prepare images for gatsby
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 960,
+            },
+          },
+          // display embedded content (f.e. youtube videos)
+          {
+            resolve: `gatsby-remark-responsive-iframe`,
+            options: {
+              wrapperStyle: `margin-bottom: 1.0725rem`,
+            },
+          },
+          // display embedded code fragments
+          {
+            resolve: `gatsby-remark-vscode`,
+          },
+          // copy files (i.e. images) to public folder during build process
+          // for production
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+          },
+          // improve syntax
+          {
+            resolve: `gatsby-remark-smartypants`,
+          },
+        ],
+        plugins: [`gatsby-remark-images`],
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
